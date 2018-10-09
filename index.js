88eba956266e39246358eeb49741a9b591d80496
Frank Roeder <3roeder@informatik.uni-hamburg.de>
2018-10-09 21:31:16 +0200

    Body parsing and error handling

--- index.js
+++ index.js
@@ -4,23 +4,42 @@ const app = express()
 const RedisAccess = require('./src/redisAccess.js')
 const redisAcccess = new RedisAccess()
 
+const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const port = process.env.PORT || 8080
 
 app.use(morgan('dev'))
+app.use(bodyParser.json())
 
-app.get('/matchserver', async (req, res) => {
+app.post('/matchserver', async (req, res) => {
   // Put Matchserver details into redis
-  const result = await redisAcccess.listPush('1player', '127.0.0.1:2448')
-  console.log(result)
-  res.json(result)
+  let ip, count
+  if (req.body.ip && req.body.playerCount) {
+    [ip, count] = [req.body.ip, req.body.playerCount]
+    const result = await redisAcccess.listPush(count + 'player', ip)
+      .catch(err => {
+        console.log(err)
+        res.status(500).end()
+      })
+    res.json(result)
+  } else {
+    res.status(400).end()
+  }
 })
 
-app.get('/joinmatch', async (req, res) => {
-  let result = await redisAcccess.listPop('1player')
-  result = result.split(':')
-  // Pop Matchserver details from redis
-  res.json({ 'IP': result[0], 'PORT': result[1] })
+app.get('/joinmatch/:playerCount', async (req, res) => {
+  if (req.params.playerCount) {
+    // Pop Matchserver details from redis list
+    let result = await redisAcccess.listPop(req.params.playerCount + 'player')
+    if (result) {
+      result = result.split(':')
+      res.json({ 'ip': result[0], 'port': result[1] })
+    } else {
+      res.status(404).send('No match found')
+    }
+  } else {
+    res.status(400).end()
+  }
 })
 
 if (redisAcccess.init()) {
